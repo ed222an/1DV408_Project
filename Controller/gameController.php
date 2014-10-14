2aec6422fb9af0172500495ceb117ae873a517be
Emil Dannberger <ed222an@student.lnu.se>
2014-10-14 18:39:12 +0200

    User can choose a hand.
    User can now choose a hand, continue with TODO's in gameController.php

--- Controller/gameController.php
+++ Controller/gameController.php
@@ -3,6 +3,14 @@
 	require_once("./view/gameView.php");
 	require_once("./model/gameModel.php");
 	
+	/* TODO: CREATE A MODEL OBJECT WITH THE CHOSEN HAND.
+	 * TODO: CREATE GAMERULES FOR THE HANDS.
+	 * TODO: CREATE RANDOMIZED HAND FOR THE COMPUTER.
+	 * TODO: USER VS. COMPUTER, CALCULATE RESULT.
+	 * TODO: PRESENT RESULT & OPTIONS.
+	 * TODO: SAVE SCORE.
+ 	*/
+	
 	class GameController
 	{
 		private $gameView;
@@ -16,6 +24,29 @@
 		
 		public function doGameControl()
 		{
+			// If user chose a hand...
+			if($this->gameView->userChoseHand())
+			{
+				// Get the name of that hand and create a model object of it.
+				$chosenHand = $this->gameView->getChosenHand();
+				var_dump($chosenHand);
+			}
+			
+			// If the user clicked the play button...
+			if($this->gameView->userClickedPlay())
+			{
+				// Return the play-HTML.
+				return $this->gameView->showContents($this->gameView->getPlayHTML());
+			}
+			
+			// If the user clicked the instructions button...
+			if($this->gameView->userClickedInstructions())
+			{
+				// Return the instructions HTML.
+				return $this->gameView->showContents($this->gameView->getInstructionsHTML());
+			}
+			
+			// Return the startmenu per default.
 			return $this->gameView->showContents();
 		}
 	}
--- View/gameView.php
+++ View/gameView.php
@@ -8,20 +8,24 @@
 		// String dependencies.
 		private $play = "play";
 		private $instructions = "instructions";
-		private $instructionsImageDirectory = "./Images/rpsls.jpeg";
-		private $rockImageDirectory = "./Images/rock.png";
-		private $paperImageDirectory = "./Images/paper.png";
-		private $scissorsImageDirectory = "./Images/scissors.png";
-		private $lizardImageDirectory = "./Images/lizard.png";
-		private $spockImageDirectory = "./Images/spock.png";
+		private $imageDirectory = "./Images/";
+		private $imageFileType = ".png";
+		private $instructionsImage = "rpsls";
+		private $rockImage = "rock";
+		private $paperImage = "paper";
+		private $scissorsImage = "scissors";
+		private $lizardImage = "lizard";
+		private $spockImage = "spock";
+		private $postSuffixX = "_x";
+		private $postSuffixY = "_y";
 		
 		public function __construct(GameModel $gameModel)
 		{
 			$this->gameModel = $gameModel;
-			$this->handImages = array($this->rockImageDirectory, $this->paperImageDirectory, $this->scissorsImageDirectory, $this->lizardImageDirectory, $this->spockImageDirectory);
+			$this->handImages = array($this->rockImage, $this->paperImage, $this->scissorsImage, $this->lizardImage, $this->spockImage);
 		}
 		
-		private function userClickedPlay()
+		public function userClickedPlay()
 		{
 			if(isset($_GET[$this->play]))
 			{
@@ -29,7 +33,7 @@
 			}
 		}
 		
-		private function userClickedInstructions()
+		public function userClickedInstructions()
 		{
 			if(isset($_GET[$this->instructions]))
 			{
@@ -37,71 +41,83 @@
 			}
 		}
 		
-		public function showContents()
+		public function userChoseHand()
 		{
-			// Default contents, shows menu.
-			$contents =	"
-							<div>
-								<a href=?$this->play>Play game</a>
-							</div>
-							<div>
-								<a href=?$this->instructions>Instructions</a>
-							</div>
-						";
-				
-			// Checks if user pressed "Play".
-			if($this->userClickedPlay())
-			{
-				// Show the gamepage.
-				$contents = "<h3>PLAY THE GAME</h3>";
-				
-				foreach($this->handImages as $hand)
+			foreach($this->handImages as $hand)
+			{	
+				if(isset($_POST[$hand . $this->postSuffixX], $_POST[$hand . $this->postSuffixY]))
 				{
-					$contents .= "<img src='$hand' alt='Image of the $hand hand'/>";
+					return TRUE;
 				}
-				
-				$contents .= "
-								<div>
-									<a href=?>Return</a>
-								</div>
-							";
 			}
+		}
+		
+		public function getChosenHand()
+		{
+			// Gets the chosen hand from the $_POST-array and strips it of its _x/_y suffix.
+			reset($_POST);
+			$chosenHand = key($_POST);
+			$chosenHand = str_replace($this->postSuffixX, "", $chosenHand);
+			$chosenHand = str_replace($this->postSuffixY, "", $chosenHand);
 			
-			// Checks if user pressed "Instructions".
-			if($this->userClickedInstructions())
+			return $chosenHand;
+		}
+		
+		public function showContents($newContents = NULL)
+		{
+			// Overrides the default menu if there are other contents to be shown.
+			if(isset($newContents) === TRUE && $newContents != "")
 			{
-				// Show the instructions.	
-				$contents = "
-								<h3>INSTRUCTIONS</h3>
-								<div>
-									<img src='$this->instructionsImageDirectory' alt='Image of the gamerules'/>
-								</div>
-								<div>
-									<ol>
-										<li>Press the 'Play'-button to start a new game.</li>
-										<li>Choose the hand you would like to play. Each hand has its strengths and weaknesses, see image above.</li>
-										<li>Your opponent then chooses a hand to play against you, not knowing what you picked.</li>
-										<li>The battle takes place! Both hands are revealed and the result is calculated.</li>
-										<li>The winner is announced!</li>
-										<li>Play again or return to the start page.</li>
-									</ol>
-								</div>
-								<div>
-									<a href=?>Return</a>
-								</div>
-							";
+				$contents = $newContents;
+			}
+			else
+			{
+				// Default contents, shows menu.
+				$contents =	"<div><a href=?$this->play>Play game</a></div>
+						<div><a href=?$this->instructions>Instructions</a></div>";
 			}
 				
 			// Returnstring.
-			$ret = "";
-			
-			$ret .= "	
-						<h1>Rock, Paper, Scissors, Lizard, Spock!</h1>
-						<h2>A PHP-game by Emil Dannberger</h2>
-						$contents
-					";
+			$ret = "<h1>Rock, Paper, Scissors, Lizard, Spock!</h1>
+					<h2>A PHP-game by Emil Dannberger</h2>
+					$contents";
 			
 			return $ret;
+		}
+		
+		// Gets the play HTML.
+		public function getPlayHTML()
+		{
+			$playHTML = "<h3>CHOOSE A HAND!</h3>";
+					
+			foreach($this->handImages as $hand)
+			{	
+				$playHTML .= "<form METHOD='post' action=''>
+								<input name='$hand' type='image' src='" . $this->imageDirectory . $hand . $this->imageFileType . "' alt='Submit Form, image of the $hand hand' />
+							</form>";
+			}
+					
+			$playHTML .= "<div><a href=?>Return</a></div>";
+			
+			return $playHTML;
+		}
+		
+		// Gets the instructions HTML.
+		public function getInstructionsHTML()
+		{	
+			$instructionsHTML = "<h3>INSTRUCTIONS</h3>
+							<div><img src='" . $this->imageDirectory . $this->instructionsImage . $this->imageFileType ."' alt='Image of the gamerules'/></div>
+							<div><ol>
+									<li>Press the 'Play'-button to start a new game.</li>
+									<li>Choose the hand you would like to play. Each hand has its strengths and weaknesses, see image above.</li>
+									<li>Your opponent then chooses a hand to play against you, not knowing what you picked.</li>
+									<li>The battle takes place! Both hands are revealed and the result is calculated.</li>
+									<li>The winner is announced!</li>
+									<li>Play again or return to the start page.</li>
+								</ol></div>
+							<div><a href=?>Return</a></div>";
+			
+			return $instructionsHTML;
 		}
 	}
 ?>
